a2fcf3de7f717419c067c60617bd13c89e11a0b9
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2022-08-12 07:51:39 +0530

    Project added to central repository

---
+++ src/main/java/com/bikkadIT/SpingBootCrudWithRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootCrudWithRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootCrudWithRestfulApplication.class, args);
+	}
+
+}
